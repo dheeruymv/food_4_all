54e6dd339c69163e09230eec5072ad84d6394a69
guangmeiye <47375532+guangmeiye@users.noreply.github.com>
2022-09-28 20:35:46 -0400

    flask ui framwork

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # food_4_all
 Food 4 all application for IBM call for Code contest
+
+python -m venv venv
+. venv/Scripts/activate
+pip install flask
+export FLASK_APP=application
+flask run
--- application.py
+++ application.py
@@ -22,21 +22,3 @@ def fav_receipies():
 @app.route('/profile')
 def profile():
     return render_template('profile.html')
-
-# @app.route('/contact', methods = ['GET','POST'])
-# def contact():
-#     if request.method == 'POST':
-#         name = request.form['name']
-#         email = request.form['email']
-#         phone = request.form['phone']
-#         message = request.form['message']
-#         return redirect(url_for('profile.html'))
-#     else:
-#         return render_template('post.html')
-
-# @app.route('/database', methods=['GET', 'POST']) 
-# def database():
-#     query = []
-#     for i in session.query(models.BlogPost):
-#         query.append((i.title, i.post, i.date))
-#     return render_template('database.html', query = query)
